9249548d0a35d12a2152a49b8dc969dcc7c0930c
riku-tamura <riku9244@gmail.com>
2025-01-23 20:19:52 +0900

    MVVM実装　

---
+++ iOSEngineerCodeCheck/ViewModel/RepositoryDetailViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  RepositoryDetailViewModel.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 田村梨恭 on 2025/01/23.
+//  Copyright © 2025 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryDetailViewModel {
+    private let repository: Repository
+    
+    // 初期化
+    init(repository: Repository) {
+        self.repository = repository
+    }
+    
+    // リポジトリ名
+    var fullName: String {
+        return repository.fullName
+    }
+    
+    // プログラミング言語
+    var language: String {
+        return "言語: \(repository.language ?? "不明")"
+    }
+    
+    // スター数
+    var stars: String {
+        return "\(repository.stargazersCount) stars"
+    }
+    
+    // ウォッチャー数
+    var watchers: String {
+        return "\(repository.watchersCount) watchers"
+    }
+    
+    // フォーク数
+    var forks: String {
+        return "\(repository.forksCount) forks"
+    }
+    
+    // オープンな issue 数
+    var openIssues: String {
+        return "\(repository.openIssuesCount) open issues"
+    }
+    
+    // アバター画像の URL
+    var avatarURL: URL? {
+        guard let avatarURL = repository.owner?.avatarURL else { return nil }
+        return URL(string: avatarURL)
+    }
+}
